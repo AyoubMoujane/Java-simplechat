0d8d305897005e01f524ca4e03a38c71d6296372
AyoubMoujane <moujane.ayoub@hotmail.com>
2020-10-28 22:21:00 +0100

    Implemented serverconsole structure just like how it's done for the client (problem with method handleMessageFromServerUI)

---
+++ simplechat2/ServerConsole.java
@@ -0,0 +1,100 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import client.ChatClient;
+import common.*;
+
+public class ServerConsole implements ChatIF {
+	
+	  //Instance variables **********************************************
+
+	/**
+	   * The instance of the server that created this ServerChat.
+	   */
+	  EchoServer server;
+	  //Class variables *************************************************
+	  
+	  /**
+	   * The default port to listen on.
+	   */
+	  final public static int DEFAULT_PORT = 5555;
+	  
+	//Constructors ****************************************************
+
+	  /**
+	   * Constructs an instance of the ServerConsole UI.
+	   *
+	   * @param host The host to connect to.
+	   * @param port The port to connect on.
+	   */
+	  public ServerConsole(int port) 
+	  {
+	      server = new EchoServer(port);
+	  }
+	  
+	  /**
+	   * This method overrides the method in the ChatIF interface.  It
+	   * displays a message onto the screen.
+	   *
+	   * @param message The string to be displayed.
+	   */
+	  public void display(String message) 
+	  {
+	    System.out.println("> " + message);
+	  }
+	  
+	  /**
+	   * This method waits for input from the console.  Once it is 
+	   * received, it sends it to the client's message handler.
+	   */
+	  public void accept() 
+	  {
+	    try
+	    {
+	      BufferedReader fromConsole = 
+	        new BufferedReader(new InputStreamReader(System.in));
+	      String message;
+
+	      while (true) 
+	      {
+	        message = fromConsole.readLine();
+	        server.handleMessageFromServerUI(message);
+	      }
+	    } 
+	    catch (Exception ex) 
+	    {
+	      System.out.println
+	        ("Unexpected error while reading from console!");
+	    }
+	  }
+	  
+	  public static void main(String[] args) 
+	  {
+		  
+	    int port = 0; //Port to listen on
+
+	    try
+	    {
+	      port = Integer.parseInt(args[0]); //Get port from command line
+	    }
+	    catch(Throwable t)
+	    {
+	      port = DEFAULT_PORT; //Set port to 5555
+	    }
+		
+	    EchoServer server = new EchoServer(port);
+	    
+	    try 
+	    {
+	      server.listen(); //Start listening for connections
+	    } 
+	    catch (Exception ex) 
+	    {
+	      System.out.println("ERROR - Could not listen for clients!");
+	    }
+	    ServerConsole chat= new ServerConsole(port);
+	    chat.accept();  //Wait for console data
+	  }
+
+}
